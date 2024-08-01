f01679108e71f61aaaa1e782e3613fca205251cd
LimonHog <153211989+LimonHog@users.noreply.github.com>
2024-08-01 18:48:28 +0300

    publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,5 +7,10 @@ load_dotenv()
 bot_token = os.getenv('TELEGRAM_TOKEN')
 bot = telegram.Bot(token=bot_token)
 
+print(bot.get_me())
+
 updates = bot.get_updates()
-bot.send_message(text='Кря', chat_id=1151243483)
+print(updates[0])
+
+updates = bot.get_updates()
+bot.send_message(text='Кря', chat_id=-1002162061613)
