b9455c49891c7a07b68a0368ca361a82ca405265
LimonHog <153211989+LimonHog@users.noreply.github.com>
2024-08-13 18:36:39 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,11 +6,9 @@ import os
 load_dotenv()
 bot_token = os.getenv('TELEGRAM_TOKEN')
 bot = telegram.Bot(token=bot_token)
+chat_id="@download_photos"
 
-print(bot.get_me())
+bot.send_message(text='Кря', chat_id=chat_id)
+# with open("images/spaceX1.jpg", 'rb') as photo_id:
+#     bot.send_photo(chat_id=chat_id, photo=photo_id, timeout = 100)
 
-updates = bot.get_updates()
-print(updates[0])
-
-updates = bot.get_updates()
-bot.send_message(text='Кря', chat_id=-1002162061613)
