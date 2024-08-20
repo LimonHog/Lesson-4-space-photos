abe3031cfd551e4327c5568c67f747513fdb1875
LimonHog <153211989+LimonHog@users.noreply.github.com>
2024-08-20 18:17:36 +0300

    Написал READVE.md и настроил отправку фото

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,9 @@
 import telegram
 from dotenv import load_dotenv
 import os
+import random
+import time
+import argparse
 
 
 load_dotenv()
@@ -11,4 +14,24 @@ chat_id="@download_photos"
 bot.send_message(text='Кря', chat_id=chat_id)
 # with open("images/spaceX1.jpg", 'rb') as photo_id:
 #     bot.send_photo(chat_id=chat_id, photo=photo_id, timeout = 100)
+
+parser = argparse.ArgumentParser(description='Отправляет случайное изобранеи в Телеграм')
+parser.add_argument(
+    '--frequence',
+    type=int,
+    default=14400,
+    help='provide an integer (default: 14400)'
+)
+parser_period = parser.parse_args()
+
+while True:
+    for dirpath, dirnames, filenames in os.walk('images'):
+     
+        random.shuffle(filenames)
+        bot.send_message(text=filenames[0], chat_id=chat_id)#не забудь исправит на вариант с изобр :3
+#        with open(f"images/{filenames[0]}", 'rb') as photo_id:
+#           bot.send_photo(chat_id=chat_id, photo=photo_id)
+        time.sleep(2)
+    time.sleep(parser_period.frequence)
+    break
 
